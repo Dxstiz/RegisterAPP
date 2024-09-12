df556149e1dc25a7b6f06033045bbe9556fa78fd
Dxstiz <143524146+Dxstiz@users.noreply.github.com>
2024-09-12 18:30:53 -0300

    Implementacion inicial de firebase
    si

---
+++ environment.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyAIgjinoNa0ZPUpWkNBtqA0wXUulGykPRw",
+  authDomain: "registerapp-82165.firebaseapp.com",
+  projectId: "registerapp-82165",
+  storageBucket: "registerapp-82165.appspot.com",
+  messagingSenderId: "601342409122",
+  appId: "1:601342409122:web:c4ea93123dee0d42d25afb",
+  measurementId: "G-P46KTFDR10"
+  }
+};
+// Initialize Firebase
+
